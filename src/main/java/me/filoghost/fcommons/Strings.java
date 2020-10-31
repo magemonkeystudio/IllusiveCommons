3593676bdcd389855bd9535406f59ca8104ed165
filoghost <filoghost@gmail.com>
2020-10-31 23:58:44 +0100

    Adjust Strings split/trim methods

--- src/main/java/me/filoghost/fcommons/Strings.java
+++ src/main/java/me/filoghost/fcommons/Strings.java
@@ -5,22 +5,72 @@
  */
 package me.filoghost.fcommons;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Strings {
 
 
-    public static String[] trimmedSplit(String string, String pattern) {
-        return trimmedSplit(string, pattern, 0);
-    }
-
-
-    public static String[] trimmedSplit(String string, String pattern, int limit) {
-        String[] output = string.split(pattern, limit);
-        for (int i = 0; i < output.length; i++) {
-            output[i] = output[i].trim();
+    public static String[] trim(String... strings) {
+        String[] result = new String[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            result[i] = strings[i].trim();
         }
-        return output;
+        return result;
     }
 
+    /**
+     * Splits a string without using regular expressions and keeps empty trailing strings.
+     */
+    public static String[] split(String string, String delimiter) {
+        return split(string, delimiter, 0);
+    }
+
+    /**
+     * Splits a string without using regular expressions and keeps empty trailing strings.
+     */
+    public static String[] split(String string, String delimiter, int limit) {
+        Preconditions.notNull(string, "string");
+        Preconditions.checkArgument(!delimiter.isEmpty(), "delimiter cannot be empty");
+        Preconditions.checkArgument(limit >= 0, "limit cannot be negative");
+
+        if (string.isEmpty() || limit == 1) {
+            // Optimization for trivial cases where no splits would occur
+            return new String[]{string};
+        }
+
+        List<String> result = new ArrayList<>();
+        int fromIndex = 0;
+        int matchIndex;
+
+        while ((matchIndex = string.indexOf(delimiter, fromIndex)) != -1) {
+            if (limit > 0 && result.size() >= limit - 1) {
+                // Limit reached (keep one slot for the remaining part)
+                break;
+            }
+
+            result.add(string.substring(fromIndex, matchIndex));
+            fromIndex = matchIndex + delimiter.length();
+        }
+
+        if (result.isEmpty()) {
+            // No match found, return the full input string
+            return new String[]{string};
+        }
+
+        // Add the remaining part of the string
+        result.add(string.substring(fromIndex));
+
+        return result.toArray(new String[0]);
+    }
+
+    public static String[] splitAndTrim(String string, String delimiter) {
+        return splitAndTrim(string, delimiter, 0);
+    }
+
+    public static String[] splitAndTrim(String string, String delimiter, int limit) {
+        return trim(split(string, delimiter, limit));
+    }
 
     public static String stripChars(String string, char... charsToRemove) {
         if (isEmpty(string) || charsToRemove.length == 0) {
---
+++ src/test/java/me/filoghost/fcommons/StringsTest.java
@@ -0,0 +1,67 @@
+package me.filoghost.fcommons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StringsTest {
+
+    @Test
+    void trim() {
+        String[] input = {" a", " a ", "a "};
+        assertThat(Strings.trim(input)).containsExactly("a", "a", "a");
+    }
+
+    @Test
+    void trimPreserveInput() {
+        String[] input = {" a "};
+        Strings.trim(input);
+        assertThat(input).containsExactly(" a ");
+    }
+
+    @Test
+    void splitNoRegex() {
+        assertThat(Strings.split("ab.cd", ".")).containsExactly("ab", "cd");
+    }
+
+    @Test
+    void splitEmptyParts() {
+        assertThat(Strings.split("/ab///cd/", "/")).containsExactly("", "ab", "", "", "cd", "");
+    }
+
+    @Test
+    void doNotSplit() {
+        assertThat(Strings.split("ab/cd/ef", "/", 1)).containsExactly("ab/cd/ef");
+    }
+
+    @Test
+    void splitMissingDelimiter() {
+        assertThat(Strings.split("abc", "/", 2)).containsExactly("abc");
+    }
+
+    @Test
+    void splitLimit() {
+        assertThat(Strings.split("ab/cd/ef", "/", 2)).containsExactly("ab", "cd/ef");
+    }
+
+    @Test
+    void splitLimitExact() {
+        assertThat(Strings.split("ab/cd/ef", "/", 3)).containsExactly("ab", "cd", "ef");
+    }
+
+    @Test
+    void stripChars() {
+        assertThat(Strings.stripChars("./ab/./cd/.", '.', '/')).isEqualTo("abcd");
+    }
+
+    @Test
+    void capitalizeFully() {
+        assertThat(Strings.capitalizeFully("hello world\nnew line")).isEqualTo("Hello World\nNew Line");
+    }
+
+    @Test
+    void capitalizeFirst() {
+        assertThat(Strings.capitalizeFirst("hello world")).isEqualTo("Hello world");
+    }
+
+}
